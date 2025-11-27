e2672de5c09b9638fa9c45996bc4732249c42ff0
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-11-27 11:53:33 +0100

    Working on data dictionary PORSCH dataset

---
+++ projects/porsch/dependencies.R
@@ -0,0 +1,20 @@
+required_packages <- c(
+  "tidyverse",
+  "janitor"
+)
+
+install_if_missing <- function(pkgs) {
+  missing <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
+  if(length(missing)) {
+    message("Installing missing packages: ", paste(missing, collapse = ", "))
+    install.packages(missing, dependencies = TRUE)
+  }
+}
+
+install_if_missing(required_packages)
+
+invisible(lapply(required_packages, library, character.only = TRUE))
+
+for (file in list.files("R", full.names = TRUE, recursive = TRUE)) {
+  source(file)
+}
